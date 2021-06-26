5c2d9dbb7828954885938ce6ba828c702c4a9976
Jay Kao <k15682574@gmail.com>
2021-06-27 07:35:14 +0800

    finish gurufocus and beta parsing

--- DGM_parsing.py
+++ DGM_parsing.py
@@ -1,7 +1,9 @@
 import urllib.request as req
 import bs4
 import re
-url = "https://www.gurufocus.com/stock/abbv/dividend"
+
+# gurufocus start
+url = "https://www.gurufocus.com/stock/hd/dividend"
 
 request = req.Request(url, headers = {
 	"User-Agent":"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
@@ -11,11 +13,54 @@ with req.urlopen(request) as response:
 	data = response.read().decode("utf-8")
 	
 soup = bs4.BeautifulSoup(data, "lxml")
-'''
-target = soup.find("script", text = re.compile("dividend_growth_5y"))
-cut1 = target.prettify().split("dividend_growth_5y:")
-cut2 = float(cut1[1].split(",")[0])
-'''
-cut1 = soup.prettify().split("dividend_growth_5y:")[1]
-cut2 = float(cut1.split(",")[0])
-print(cut2)
+
+cut = soup.prettify().split("dividend_growth_5y:")[1]
+dividend_growth_5y = float(cut.split(",")[0])
+print("dividend_growth_5y is", dividend_growth_5y)
+
+cut = soup.prettify().split(",yield:")[2]
+dividend_yield = float(cut.split(",")[0])
+print("dividend_yield is", dividend_yield)
+
+cut = soup.prettify().split(",dividend_growth_10y:")[1]
+dividend_growth_10y = 0
+if cut.split(",")[0] != 'a':
+	dividend_growth_10y = float(cut.split(",")[0])
+print("dividend_growth_10y is", dividend_growth_10y)
+# gurufocus end
+
+# yahoo start
+url = "https://finance.yahoo.com/quote/HD?p=HD&.tsrc=fin-srch"
+
+request = req.Request(url, headers = {
+	"User-Agent":"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+})
+
+with req.urlopen(request) as response:
+	data = response.read().decode("utf-8")
+	
+soup = bs4.BeautifulSoup(data, "lxml")
+
+cut = soup.prettify().split(",\"beta\":{")[1]
+cut = cut.split("\"fmt\":\"")[1]
+beta = float(cut.split("\"},")[0])
+print("beta is", beta)
+# yahoo end
+
+# morningstar start
+url = "https://www.morningstar.com/stocks/xnys/hd/quote"
+
+request = req.Request(url, headers = {
+	"User-Agent":"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+})
+
+with req.urlopen(request) as response:
+	data = response.read().decode("utf-8")
+	
+soup = bs4.BeautifulSoup(data, "lxml")
+
+cut = soup.prettify().split(",\"beta\":{")[1]
+cut = cut.split("\"fmt\":\"")[1]
+beta = float(cut.split("\"},")[0])
+print("beta is", beta)
+# morningstar end
