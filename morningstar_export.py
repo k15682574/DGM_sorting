1322ce7ecc321fbe4a12cfdf15370b3a3737110c
Jay Kao <k15682574@gmail.com>
2021-07-18 19:18:58 +0800

    export .csv file from morningstar keyratio page

---
+++ morningstar_export.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+
+stock = 'BTI'
+
+driver = webdriver.Chrome('chromedriver')
+while True:
+    try:
+        driver.get('https://financials.morningstar.com/ratios/r.html?t=' + stock)
+        large_button = driver.find_element_by_class_name("large_button")
+        large_button.click()
+        break
+    except:
+        print("export " + stock + " failed, try again")
+        continue
+
+stock = 'AMZN'
+while True:
+    try:
+        driver.get('https://financials.morningstar.com/ratios/r.html?t=' + stock)
+        large_button = driver.find_element_by_class_name("large_button")
+        large_button.click()
+        break
+    except:
+        print("export " + stock + " failed, try again")
+        continue
